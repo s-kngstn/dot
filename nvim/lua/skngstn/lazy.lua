36e1770d077ca3abf88b43ef0f9b67aecdd80311
s-kngstn <s.kngstn@protonmail.com>
2024-03-19 09:14:41 +0000

    ignore lazy.lock

--- nvim/lua/skngstn/lazy.lua
+++ nvim/lua/skngstn/lazy.lua
@@ -11,4 +11,17 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("skngstn.plugins")
+require("lazy").setup({
+  {import = "skngstn.plugins"},
+}, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+  install = {
+    colorscheme = { "tokyonight" },
+  },
+})
--- nvim/lua/skngstn/plugins/neoscroll.lua
+++ nvim/lua/skngstn/plugins/neoscroll.lua
@@ -1,6 +1,5 @@
 return {
   "karb94/neoscroll.nvim",
-  config = function ()
-    require('neoscroll').setup {}
-  end
+  event = {"BufRead", "BufNewFile"},
+  config = true
 }
