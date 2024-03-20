f4d64bf513d2c8bb75d2fb9d026c59cd2bc654a4
Sam Kingston <s.kngstn@protonmail.com>
2024-03-20 07:24:01 +0000

    feat: dismiss copilot keymap'

--- nvim/lua/skngstn/plugins/copilot.lua
+++ nvim/lua/skngstn/plugins/copilot.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     vim.cmd([[
       imap <silent><expr> <C-J> copilot#Accept("\<CR>")
+      imap <silent><expr> <C-K> copilot#Dismiss()
       let g:copilot_no_tab_map = v:true
     ]])
   end,
